c5d9db7c2539a44e90e2d0352ee83ee1dfca92b8
Ana Claudia <ana.felippe3@etec.sp.gov.br>
2022-11-14 17:47:01 -0300

    versão final

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.text.isDigitsOnly
 import com.example.android.diceroller.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+/*class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         val countButton: Button = findViewById(R.id.count_up)
         countButton.setOnClickListener { countup() }
 
-    }
-    private fun rollDice(){
+    }*/
+    /*private fun rollDice(){
         //val resultText: TextView = findViewById(R.id.result_text)
         //resultText.text = "Dice Rolled!"
 
@@ -36,15 +33,17 @@ class MainActivity : AppCompatActivity() {
             binding.resultText.text = randomInt.toString()
         }else{
             binding.resultText.text = "Dice Roller!"
-        }
+        }*/
 
 
 
         /*Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()*/
-    }
+            Toast.LENGTH_SHORT).show()
+    }*/
+
+
     //DESAFIO DO BOTÃO COUNT
-    private fun countup(){
+    /*private fun countup(){
         val resultText: TextView = findViewById(R.id.result_text)
         if(resultText.text == "Dice Roller!"){
             resultText.text = "1"
@@ -59,6 +58,45 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+}*/
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Rolou o dado!", Toast.LENGTH_SHORT).show()
+
+            rollDice()
+        }
+    }
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
 
